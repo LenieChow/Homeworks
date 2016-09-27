30c7600c5b1434ee803024820e8458c3430b2772
lenie chow <leniechow@gmail.com>
2016-09-27 22:27:14 +0800

    basic functionality in place

--- JS/script.js
+++ JS/script.js
@@ -1,56 +1,123 @@
-// document.addEventListener('DOMContentLoaded',init)
-// to check if everything is working
-// function init () {
-  // set variables in here
-  // alert('script loaded')
-  // var currentPlayer = 1
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+  alert ("Lets start playing.A horizontal, vertical or diagonal row will make you the winner!")
+
+    var currentPlayer = 1;
+
+
+
+
+    var smallbox = document.getElementsByClassName('smallbox');
+    for (var i = 0; i < smallbox.length; i++) {
+        smallbox[i].addEventListener('click', boxClick)
+    }
+    // What happens when a box is clicked
+    function boxClick() {
+
+        console.log(this.className);
+        if (currentPlayer === 1) {
+            this.innerHTML = 'X';
+            this.classList.add('clickX')
+            if(checkWin("clickX")){
+              alert("Player X won!");
+              resetGame();
+            }
+          } else {
+            this.innerHTML = 'O';
+            this.classList.add('clickO')
+            if(checkWin("clickO")){
+              alert("Player O won!");
+              resetGame();
+            }
+
+        }
+        // calling function
+
+        switchPlayer();
+
+    }
+
+    // How to switch between players
+    function switchPlayer() {
+        if (currentPlayer === 1) {
+            currentPlayer = 2;
+        } else {
+            currentPlayer = 1;
+        }
+    }
+
+
+
+    // Label all boxes to retrieve it
+    var smallbox = document.getElementsByClassName('smallbox');
+    var zero = document.getElementById("Box0");
+    var one = document.getElementById("Box1");
+    var two = document.getElementById("Box2");
+    var three = document.getElementById("Box3");
+    var four = document.getElementById("Box4");
+    var five = document.getElementById("Box5");
+    var six = document.getElementById("Box6");
+    var seven = document.getElementById("Box7");
+    var eight = document.getElementById("Box8");
+
+
+
+
+    function checkWin(playerType) {
+        var winner = false;
+        //  if ( zero.classList.contains('clickX') &&  one.classList.contains('clickX') && two.classList.contains('clickX')){
+        //      alert("Player 1 wins!");
+
+        //check if clickX is class of box0 compare it with class of box1 then comapre it with class of Box2
+        if (zero.classList.contains(playerType) && one.classList.contains(playerType) && two.classList.contains(playerType)) {
+            winner = true;
+            // alert(playerType + "wins!");
+
+        }else if (three.classList.contains(playerType) && four.classList.contains(playerType) && five.classList.contains(playerType)) {
+            winner = true;
+
+        } else if (six.classList.contains(playerType) && seven.classList.contains(playerType) && eight.classList.contains(playerType)) {
+            winner = true;
+
+        } else if (zero.classList.contains(playerType) && three.classList.contains(playerType) && six.classList.contains(playerType)) {
+
+            winner = true;
+
+        } else if (one.classList.contains(playerType) && four.classList.contains(playerType) && seven.classList.contains(playerType)) {
+            winner = true;
+        }else if (two.classList.contains(playerType) && five.classList.contains(playerType) && eight.classList.contains(playerType)) {
+          winner = true;
+        } else if (zero.classList.contains(playerType) && four.classList.contains(playerType) && eight.classList.contains(playerType)) {
+          winner = true;
+        } else if (two.classList.contains(playerType) && four.classList.contains(playerType) && six.classList.contains(playerType)) {
+          winner = true;
+        }
+        return winner;
+      }
+
+
+      var reset = document.querySelector("#btn")
+          reset.addEventListener("click", resetGame)
+
+
+          function resetGame(){
+            location.reload();
+          }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
-// Define small box.Targetting the elements - each box on the gameboard. can attach listeners to each element
-// var smallbox = document.getElementsByClassName('smallbox')
-// Check on dev tools to make sure I am targetting the right elements. Check output everytime.
-// console.log(smallbox)
-// for loop to add listeners to each box since the class has many elements
-// for (var i = 0; i < smallbox.length; i++) {
-//   smallbox[i].addEventListener('click', boxClick) () {
-//     console.log('Click worked')
-//     this.textContent = 'x'
-//   })
-// }
-
-// check each element is targetted and define your function
-// for (var i=0; i<smallbox.length; i++) {
-  // smallbox[i].addEventListener('click',boxClick)
-}
-// function boxClick () {
-//   alert('box is clicked')
-// }
-// To check if each box really is working, set id on each box.
-// function boxClick() {
-//   alert('smallbox'+this.id+ 'is clicked')
-//
-// }
-// the above is all making the event listeners are all working. The below is to add X in the boxes
-// function boxClick (){
-//   this.textContent='X'
-// }
-
-// function boxClick () {
-  // this.classList.add('clickX')
-  // this.classList.add('clickO')
-  // this.textContent='X'
-  // this.textContent='O'
-
-}
-
-
-
-
-
-
-
-
-
-// document.getElementById('btn').addEventListener('click', myFunction)
-// function myFunction () {
-  // alert("Let's play again! YAY!")
-// }
--- JS/script1.js
+++ JS/script1.js
@@ -1,44 +1,88 @@
-document.addEventListener('DOMContentLoaded', init)
+document.addEventListener('DOMContentLoaded', init);
 
-function init () {
+function init() {
+
+    var currentPlayer = 1;
+
+    //   var smallbox=document.getElementsByClassName('smallbox')
+    //   for (var i=0; i<smallbox.length; i++) {
+    //     var target = event.target;
+    //     if (currentPlayer==0 || currentPlayer % 2 == 0){
+    //       target.innerHTML='X'
+    //       currentPlayer += 1
+    //     }
+    //    else {
+    //      target.innerHTML='O'
+    //      currentPlayer += 1
+    //    }
+    //
+    // }
+
+
+
+    var smallbox = document.getElementsByClassName('smallbox');
+
+    function boxClick () {
+
+      if (currentPlayer === 1) {
+        // this.innerHTML='X';
+        this.classList.add('clickX')
+      } else {
+        this.classList.add('clickO')
+      }
+     switchPlayer();
+
+    }
+    function switchPlayer () {
+      if (currentPlayer === 1) {
+        currentPlayer = 2;
+      } else {
+          currentPlayer = 1;
+        }
+    }
+
+
+
+    // Declaring each box as a variable
+
+    console.log(smallbox)
+    for (var i = 0; i < smallbox.length; i++) {
+      smallbox[i].addEventListener('click', boxClick)
+        // smallbox[i].addEventListener('click', function(event) {
+        //     // var target = event.target
+        //     //
+        //     //     // 0 is always false. If current player === 0 is false, else statement will run.
+        //     //     // box will show O. += ensures that every move will add 1 so that we will get X and O
+        //     //     // sets what happens if user clicks the boxes
+        //     if (currentPlayer === 0 || currentPlayer % 2 === 0) {
+        //         this.innerHTML = 'X'
+        //             // currentPlayer += 1
+        //     } else {
+        //         this.innerHTML = 'O'
+        //             // currentPlayer += 1
+        //     }
+        // });
+    }
+
+
+
+
+
+
 }
 
-var currentPlayer = 0
 
-var smallbox = document.getElementsByClassName('smallbox')
+// set up if/else statements to assign a value to each box
 
-// Declaring each box as a variable
-
-console.log(smallbox)
-for (var i = 0; i < smallbox.length; i++) {
-  smallbox[i].addEventListener('click', function (event) {
-    var target = event.target
-
-    // 0 is always false. If current player === 0 is false, else statement will run.
-    // box will show O. += ensures that every move will add 1 so that we will get X and O
-    // sets what happens if user clicks the boxes
-    if (currentPlayer === 0 || currentPlayer % 2 === 0) {
-      this.classList.add('clickX')
-      target.innerHTML = 'X'
-      currentPlayer += 1
-    } else {
-      this.classList.add('clickO')
-      target.innerHTML = 'O'
-      currentPlayer += 1
-    }
-
-
-    // set up if/else statements to assign a value to each box
-
-  Box1.addEventListener('click' function(event)){
-
-    if (currentPlayer === 0 || currentPlayer % 2 ===0) {
-      Box1=1;
-    }
-    else {
-      Box1=2;
-    }
-  }
+// Box1.addEventListener('click' function(event)){
+//
+//   if (currentPlayer === 0 || currentPlayer % 2 ===0) {
+//     Box1=1;
+//   }
+//   else {
+//     Box1=2;
+//   }
+// }
 
 
 // Setting a value to each box with if/else statements
